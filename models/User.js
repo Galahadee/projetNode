9ccbb5a2185abd86f8cd3b16f3022118465c538b
Simon Joly <sim.joly@outlook.fr>
2023-10-06 10:23:27 +0200

    crea UsersModel

--- models/User.js
+++ models/User.js
@@ -1,11 +1,16 @@
-/*
-  Voici la structure d'un document Utilisateur sur lequel vous vous baserez pour faire le Schéma mongoose :
+import mongoose from "mongoose";
 
-  {
-    firstName  // type String, obligatoire
-    lastName  // type String, obligatoire
-    email  // type String, obligatoire
-    password  // type String, obligatoire
-  }
 
-*/
+const usersSchema=new mongoose.Schema({
+  _id: {type: mongoose.SchemaTypes.ObjectId,required: true,default: () => new mongoose.Types.ObjectId(),},
+  firstName  :{ type: String, required: true },
+  lastName :{ type: String, required: true },
+  email  :{ type: String, required: true },
+  password  :{ type: String, required: true }
+})
+
+
+const collectionName = 'users'
+const UsersModel = mongoose.model('Users', usersSchema, collectionName)
+
+export default UsersModel
