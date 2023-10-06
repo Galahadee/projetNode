3032a4951d1b81a7a4f3a0e9ba959f64562e6d72
Simon Joly <sim.joly@outlook.fr>
2023-10-06 10:35:34 +0200

    add Routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,17 @@
 import { Router } from 'express';
 import HomeController from '../controllers/home.js';
+import { LoginController, LogoutController, PostLoginController } from '../controllers/login.js';
+import DashBoardController from '../controllers/dashboard.js'
+import { authGuard, setTemplateVars } from '../middlewares/session.js';
 
 const appRouter = Router()
 
+appRouter.use(setTemplateVars)
+
 appRouter.get('/', HomeController);
+appRouter.get('/login', LoginController);
+appRouter.post('/login', PostLoginController);
+appRouter.get('/logout', authGuard, LogoutController);
+appRouter.get('/dashboard',authGuard, DashBoardController);
 
 export default appRouter;
