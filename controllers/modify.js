96029c5c87bb02a9c4146ca8390fdfcab2acaf70
Simon Joly <sim.joly@outlook.fr>
2023-10-06 16:36:05 +0200

    fix modif

--- controllers/modify.js
+++ controllers/modify.js
@@ -5,9 +5,9 @@ import argon2 from "argon2";
 
 export default async function modifController(req, res) {
     try {
-        const password = req.body.password, confPassword = req.body.password_confirm, lastName = req.body.lastName, firstName = req.body.firstName,
-            email = req.body.email;
-
+        const password = req.body.password, confPassword = req.body.password_confirm,oldPassword=req.body.old_password, lastName = req.body.lastName, firstName = req.body.firstName,
+            email = req.body.email,currUserEmail=req.session.user.email;
+        const currUser= await UsersModel.findOne({'email':currUserEmail});
         let newUser = {}, error = false;
 
         const emailUser = await UsersModel.findOne({ "email": email }), emailValidated = validateEmail(email);
@@ -23,19 +23,24 @@ export default async function modifController(req, res) {
                     ...req.session.user,
                     email: email
                 };
-                newUser = { ...newUser, email: email }
+                newUser = { ...newUser, "email": email }
                 req.flash('success', 'Email enregistré.')
             }
         }
 
         if (password) {
-            if (password !== confPassword) {
-                error = true;
-                req.flash('error', 'Les champs mots de passe ne sont pas identiques!');
+            if(await argon2.verify(currUser.password,oldPassword) ){
+                if (password !== confPassword) {
+                    error = true;
+                    req.flash('error', 'Les champs mots de passe ne sont pas identiques!');
+                } else {
+                    const hashPass = await argon2.hash(password);
+                    newUser = { ...newUser, password: hashPass };
+                    req.flash('success', "Mot de passe enregistré.")
+                }
             } else {
-                const hashPass = await argon2.hash(password);
-                newUser = { ...newUser, password: hashPass };
-                req.flash('success', "Mot de passe enregistré.")
+                error = true;
+                req.flash('error', 'Ancien mot de passe invalide !');
             }
         }
 
@@ -61,7 +66,8 @@ export default async function modifController(req, res) {
             req.flash('error', "Champs vides.")
         } else {
             try {
-                await UsersModel.findOneAndUpdate({ email: req.session.user.email }, newUser);
+                // console.log(newUser);
+                await UsersModel.findOneAndUpdate({ email: currUserEmail }, newUser);
             } catch (err) {
                 console.error('Erreur d\'insertion', err.message);
             }
