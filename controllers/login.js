f729f2cad138217486517612581a8047c927e442
Simon Joly <sim.joly@outlook.fr>
2023-10-06 10:29:55 +0200

    loginController

---
+++ controllers/login.js
@@ -0,0 +1,29 @@
+
+import { validate as validateEmail } from 'email-validator'
+
+export function LoginController(req, res) {
+  res.render('login')
+}
+
+export function PostLoginController(req, res) {
+  const emailValidated = validateEmail(req.body.email)
+  if (emailValidated) {
+    req.session.user = {
+      email: req.body.email,
+    }
+    res.redirect('dashboard')
+  }
+  else {
+    res.status(400).send(`<h1>Erreur !</h1><p>Email invalide !</p>`)
+  }
+}
+
+export function LogoutController(req, res) {
+  req.session.destroy((err) => {
+    if (err) {
+      res.status(500).send(`<h1>Erreur !</h1><p>${err.message}</p>`)
+      return
+    }
+    res.redirect('/login')
+  })
+}
